57babb49f0911ecc6b7bd526437e29bdec1f37c4
Jakub Stasiak <kubafr2@gmail.com>
2018-04-06 20:42:29 +0200

    Fix img generation - use POST instead GET

--- utils/hydrators.js
+++ utils/hydrators.js
@@ -8,10 +8,10 @@ const stashPurrImage = (z, bundle) => {
   z.console.log(`Purr url = ${purrUrl}`);
 
   const filePromise = z.request({
-    method: 'GET',
-    url: 'https://apileap.com/api/screenshot/v1/urltoimage',
+    method: 'POST',
+    url: 'https://apileap.com/api/screenshot/v1/urltoimage?fresh=true',
     raw: true,
-    params: {
+    form: {
       access_key: '2d07cafe30de41aea7efa87e6789aecd',
       url: purrUrl,
       width: 1020,
@@ -20,17 +20,23 @@ const stashPurrImage = (z, bundle) => {
     },
   });
 
-  filePromise.then((res) => {
+  return filePromise.then(res => {
     if (res.status >= 200 && res.status < 300) {
-      z.console.log('Fetching screen ok');
-    } else {
-      response.text().then(content => z.console.log(`Screen response ${content}`));
-    }
-  })
+      z.console.log(
+        `Fetching screen ok = ${res.ok}`,
+        res.getHeader('content-length')
+      );
 
-  return z.stashFile(filePromise).then(url => {
-    z.console.log(`Stashed URL = ${url}`);
-    return url;
+      return z.stashFile(filePromise).then(url => {
+        z.console.log(`Stashed URL = ${url}`);
+        return url;
+      });
+    }
+
+    return res
+      .text()
+      .then(content => z.console.log(`Screen response ${res.ok} ${content}`))
+      .then(() => Promise.resolve('http://via.placeholder.com/300'));
   });
 };
 
